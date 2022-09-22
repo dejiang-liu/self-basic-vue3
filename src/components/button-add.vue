0a78b0089073a3e34b7099f9d4d848f00a0509ea
liudejiang <liudejiangaaa@163.com>
2022-09-22 15:56:02 +0800

    init

---
+++ src/components/button-add.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="h">hellow world</div>
+  <button @click="add">add</button> {{ num }}
+</template>
+
+<script lang="ts" setup>
+import { ref } from "@vue/reactivity";
+
+let num = ref(1);
+const add = () => {
+  num.value += 1;
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+  <button-add></button-add>
+</template>
+
+<script lang="ts" setup>
+import ButtonAdd from "@/components/button-add.vue";
+</script>
